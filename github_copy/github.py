2b6dddccd224802c7e08d6cd0c42d73fa529af59
devsetgo <mike@DESKTOP-6FCGHOC.localdomain>
2020-07-26 15:30:31 -0400

    black formatting

--- github_copy/github.py
+++ github_copy/github.py
@@ -1,20 +1,23 @@
-import requests
-import json
+# -*- coding: utf-8 -*-
 import os
 import pprint
+
+import requests
 
 # Setup
 pp = pprint.PrettyPrinter(indent=4)
 # Set Github username here
-GITHUB_USERNAME: str =  'devsetgo'
-# Set maximum number of repo to loop through 
+GITHUB_USERNAME: str = "devsetgo"
+# Set maximum number of repo to loop through
 MAX_REPO: int = 200
+
 
 def setup_repos(git_url: str):
     """
     Run git clone command for repo
     """
-    os.system(f'git clone {git_url}')
+    os.system(f"git clone {git_url}")
+
 
 def call_api(url):
     """
@@ -26,10 +29,11 @@ def call_api(url):
     data = r.json()
 
     for d in data:
-        pp.pprint(d['git_url'])
-        setup_repos(d['git_url'])
-        name= d['name']
+        pp.pprint(d["git_url"])
+        setup_repos(d["git_url"])
+        name = d["name"]
         print(f"Complete clone of {name}")
+
 
 # Gets the first 200 most recent repos
 url = f"https://api.github.com/users/{GITHUB_USERNAME}/repos?sort=updated&per_page={MAX_REPO}"
