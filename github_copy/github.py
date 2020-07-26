52c23ea5f3c012559503d5e88fb5de8e2ee9fed5
devsetgo <mike@DESKTOP-6FCGHOC.localdomain>
2020-07-26 15:29:47 -0400

    Adding documentation

--- github_copy/github.py
+++ github_copy/github.py
@@ -3,34 +3,36 @@ import json
 import os
 import pprint
 
+# Setup
 pp = pprint.PrettyPrinter(indent=4)
+# Set Github username here
+GITHUB_USERNAME: str =  'devsetgo'
+# Set maximum number of repo to loop through 
+MAX_REPO: int = 200
 
 def setup_repos(git_url: str):
-    print(git_url)
+    """
+    Run git clone command for repo
+    """
     os.system(f'git clone {git_url}')
 
 def call_api(url):
+    """
+    call Github API for User
+    Loop through all repos and call
+    """
     r = requests.get(url)
     # print(r.status_code)
     data = r.json()
-    # Parse JSON data and save to file
-    # result = for_json(data)
-    # # Save full JSON response and save to file
-    # save_all_json(data)
 
-    # Print result of JSON parse
-    # print(data)
-    count = 0
     for d in data:
         pp.pprint(d['git_url'])
         setup_repos(d['git_url'])
-        # count += 1
-        # if count == 20:
-        #     break
         name= d['name']
         print(f"Complete clone of {name}")
 
-url = "https://api.github.com/users/devsetgo/repos"
+# Gets the first 200 most recent repos
+url = f"https://api.github.com/users/{GITHUB_USERNAME}/repos?sort=updated&per_page={MAX_REPO}"
 
 
 if __name__ == "__main__":
