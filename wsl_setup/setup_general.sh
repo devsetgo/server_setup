5bd6d93d0fe827ba3675d9dbeae457927572ce8d
mike <mikeryan56@gmail.com>
2020-12-30 20:06:53 -0500

    fix of path

--- wsl_setup/setup_general.sh
+++ wsl_setup/setup_general.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 # update and upgrade server
-sudo apt-get update & sudo apt-get upgrade -y
+sudo apt-get update && sudo apt-get upgrade -y
 # install Libraries
 # GIT
 sudo apt install git -y
@@ -11,15 +11,15 @@ sudo apt install git -y
 sudo apt install wget -y
 # run installers
 # Python
-./wsl/setup_python.sh
+./wsl_setup/setup_python.sh
 # Golang
-./wsl/setup_go.sh
+./wsl_setup/setup_go.sh
 # Node and NPM
-./wsl/setup_javascript.sh
+./wsl_setup/setup_javascript.sh
 # JAVA
-./wsl/setup_java.sh
+./wsl_setup/setup_java.sh
 # Hugo
-./wsl/setup_hugo.sh
+./wsl_setup/setup_hugo.sh
 # store git credentials for Github
 git config --global credential.helper store -y
 echo "General Setup - COMPLETE"
