267cb81dc18618e14499214f78c95ae7b6db4b49
mike <mikeryan56@gmail.com>
2021-01-01 19:03:47 -0500

    adding wsl-microk8s

--- wsl_setup/setup_general.sh
+++ wsl_setup/setup_general.sh
@@ -2,6 +2,7 @@
 set -e
 set -x
 
+
 # update and upgrade server
 sudo apt-get update && sudo apt-get upgrade -y
 # install Libraries
@@ -20,7 +21,27 @@ sudo apt install wget -y
 ./wsl_setup/setup_java.sh
 # Hugo
 ./wsl_setup/setup_hugo.sh
+# install microk8s
+./wsl_setup/ubuntu-wsl2-systemd-script.sh
+# Add the user to the microk8s group
+USERNAME=$(whoami)
+sudo usermod -a -G microk8s $USERNAME
+# Set the ownership of the .kube directory back to the user
+sudo chown -f -R $USERNAME ~/.kube
+
 # store git credentials for Github
+echo -n "git user name (e.g. mike): "
+read gitname
+echo -n "git user email (e.g. mike@something.com): "
+read gitemail
+echo -n "github personal token: "
+read githubtoken
+
 git config --global credential.helper store -y
+git config --global user.name $gitname
+git config --global user.email $gitemail
+git config --global user.password $githubtoken
 echo "General Setup - COMPLETE"
+echo "You will need to create a new session to use microk8s without sudo"
+
 echo "Run Python Github utility to clone all public repos"
